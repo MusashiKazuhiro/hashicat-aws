cbc550393cb9d6776da136f63f68a063a30580c7
Kazuhiro MUSASHI <kazuhiro.musashi.wy@hitachi-solutions.com>
2021-11-24 08:49:36 +0000

    modify

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,10 @@
-
 module "s3-bucket" {
   source  = "app.terraform.io/KazuhiroMusashi-training/s3-bucket/aws"
-  version = "2.11.1"
+  version = "2.2.0"
+  
 
   # insert required variables here
   bucket_prefix = "kazuhiro-musashi"
   bucket = "gaurav"
   acl    = "private"
 }
-
